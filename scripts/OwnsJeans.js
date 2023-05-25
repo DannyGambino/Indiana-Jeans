58763c1779182a1685ac254466de98168f8bd979
Daniel Cicalese <dcik02@gmail.com>
2023-05-25 15:20:49 -0400

    third commit

--- scripts/OwnsJeans.js
+++ scripts/OwnsJeans.js
@@ -1,3 +1,5 @@
+import { setOwnsBlueJeans } from "./TransientState.js"
+
 /*
     Responsibility: 
     Provide UI controls for survey taker to choose 'yes' or 'no' for
@@ -9,8 +11,24 @@
 */
 
 export const OwnJeansChoices = () => {
-    let html = "<h2>Do you own a pair of blue jeans?</h2>"
-    html += "<input type='radio' name='ownsJeans' value='true' /> Yes"
-    html += "<input type='radio' name='ownsJeans' value='false' /> No"
+
+    document.addEventListener("change", handleOwnershipChange)
+
+    let html = "<h1>Do you own a pair of blue jeans?</h1>"
+    html += "<input type='radio' name='owns' value='true' /> Yes"
+    html += "<input type='radio' name='owns' value='false' /> No"
+
     return html
 }
+
+ /*
+     Since the value of the radio buttons is the string of
+     'true' and the string of 'false', you must convert the
+     string to an actual boolean with JSON.parse() as seen below
+ */
+const handleOwnershipChange = (changeEvent) => {
+        if (changeEvent.target.name === "ownsJeans") {
+            const convertedToBoolean = JSON.parse(changeEvent.target.value)
+            setOwnsBlueJeans(convertedToBoolean)
+        }
+    }
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 import { OwnJeansChoices } from "./OwnsJeans.js"
+import { SaveSubmission } from "./SaveSubmissionComponent.js"
+import { SubmissionList } from "./Submissions.js"
 import { LocationTypeChoices } from "./UrbanDweller.js"
 
 const container = document.querySelector("#container")
@@ -6,10 +8,14 @@ const container = document.querySelector("#container")
 const render = async () => {
     const jeanOwnershipHTML = OwnJeansChoices()
     const locationsHTML = await LocationTypeChoices()
-
+    const buttonHTML = await SaveSubmission()
+    const submissionListHTML = await SubmissionList()
+    
     container.innerHTML = `
         ${jeanOwnershipHTML}
-        ${locationsHTML}`
+        ${locationsHTML}
+        ${buttonHTML}
+        ${submissionListHTML}`
 }
 
 render()
